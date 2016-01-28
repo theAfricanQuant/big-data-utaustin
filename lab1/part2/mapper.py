7b76bea9179f8bd5cffd47313c232dbc921c7617
Rigl <grimfate126@gmail.com>
2016-01-28 01:36:24 -0600

    Updated lab1 materials

---
+++ lab1/part2/mapper.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+
+# Input comes from the standard input line by line (not word by word!)
+for line in sys.stdin:
+	
+	# Dummy print. You may delete this.
+	print ""
---
+++ lab1/part2/reducer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+    
+# same as the mapper, input comes from the standard input line by line
+for line in sys.stdin:
+    
+    # Dummy print. You may delete this.
+    print ""
