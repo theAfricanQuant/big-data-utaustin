f5dc6b730a05477f73b0dfa37c74f4cee98f2781
Rohan Nagar <rohan.nagar@utexas.edu>
2016-01-31 17:30:02 -0600

    Include contraction checks in mapper

--- lab1/part1/mapper.py
+++ lab1/part1/mapper.py
@@ -8,6 +8,44 @@ for line in sys.stdin:
         words = line.split()
 
         for word in words:
-            # Output each word as the key, with 1 as the value (count of word)
-	    print "%s %s" % (word, 1)
+            # First do the contraction check
+            lower = word.lower()
+            firstWord = None
+            secondWord = None
+
+            if lower == "i\'m":
+                firstWord = word[:1]
+                secondWord = "a" + word[2:]
+            elif lower == "i\'ve":
+                firstWord = word[:1]
+                secondWord = "ha" + word[2:]
+            elif lower == "aren\'t":
+                firstWord = word[:3]
+                secondWord = word[3] + "o" + word[5]
+            elif lower == "can\'t":
+                firstWord = word[:3]
+                secondWord = "no" + word[4]
+            elif lower == "didn\'t":
+                firstWord = word[:3]
+                secondWord = word[3] + "o" + word[5]
+            elif lower == "couldn\'t":
+                firstWord = word[:5]
+                secondWord = word[5] + "o" + word[7]
+            elif lower == "shouldn\'t":
+                firstWord = word[:6]
+                secondWord = word[6] + "o" + word[8]
+            elif lower == "isn\'t":
+                firstWord = word[:2]
+                secondWord = word[2] + "o" + word[4]
+            elif lower == "you\'re":
+                firstWord = word[:3]
+                secondWord = "a" + word[-2:]
+
+            if firstWord:
+	        # firstWord and secondWord must always both be populated, so we only need to check for one
+                print "%s %s" % (firstWord, 1)
+	        print "%s %s" % (secondWord, 1)
+            else:
+                # Output the word as the key, with 1 as the value (count of word)
+	        print "%s %s" % (word, 1)
 
