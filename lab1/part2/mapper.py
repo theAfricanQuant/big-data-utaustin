2ae032e2c30496cab8ee2ad31b835218f402c686
Rohan Nagar <rohan.nagar@utexas.edu>
2016-01-31 19:06:37 -0600

    Add code for unique bigram count

--- lab1/part2/mapper.py
+++ lab1/part2/mapper.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 import sys
 
+lastWord = None
+
 # Input comes from the standard input line by line (not word by word!)
 for line in sys.stdin:
-	
-	# Dummy print. You may delete this.
-	print ""
+    line = line.strip().lower()
+    words = line.split()
+
+    for i in range(len(words)):
+        if lastWord:
+            print "%s %s %s" % (lastWord, words[i], 1)
+        lastWord = words[i]
+
--- lab1/part2/reducer.py
+++ lab1/part2/reducer.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 import sys
-    
+
+currentBigram = None
+totalCount = 0
+
 # same as the mapper, input comes from the standard input line by line
 for line in sys.stdin:
-    
-    # Dummy print. You may delete this.
-    print ""
+    # Get the bigram by taking the string without the count value
+    bigram = line[:-2]
+
+    if bigram == currentBigram:
+        continue
+    else:
+        if currentBigram:
+            totalCount += 1
+        currentBigram = bigram
+
+# Don't forget the last one
+if currentBigram:
+    totalCount += 1
+
+print "%s" % (totalCount)
+
