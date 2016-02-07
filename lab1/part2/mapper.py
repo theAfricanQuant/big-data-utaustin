1a493ff1851d9adb1186edd3d718a012de4e3aad
Rohan Nagar <rohan.nagar@utexas.edu>
2016-02-07 13:08:23 -0600

    Update bigram count to only count per line

--- lab1/part2/mapper.py
+++ lab1/part2/mapper.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import sys
 
-lastWord = None
-
 # Input comes from the standard input line by line (not word by word!)
 for line in sys.stdin:
+    lastWord = None
+
     line = line.strip().lower()
     words = line.split()
 
