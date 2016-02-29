138b2b59ab3b4cb5f7556f60494053531bdf2d33
Rohan Nagar <rohan.nagar@utexas.edu>
2016-02-28 21:38:28 -0600

    Setup for part 2

--- lab2/part2/movielens.py
+++ lab2/part2/movielens.py
@@ -6,18 +6,20 @@ from mrjob.step import MRStep
 class MRMovielens(MRJob):
 
     def steps(self):
-		# This function defines the steps your job will follow. If you want to chain jobs, you can just have multiple steps.
+	# This function defines the steps your job will follow. If you want to chain jobs, you can just have multiple steps.
         return [
-			# Running the same MapReduce job twice
+	    # Running the same MapReduce job twice
             MRStep(mapper=self.mapper1, reducer=self.reducer1), 		
             MRStep(mapper=self.mapper1, reducer=self.reducer1),		
         ]
 
+
     def mapper1(self, _, value):
-		return None
+        return None
+
 
     def reducer1(self, key, value):
-		return None
+	return None
 
 
 if __name__ == '__main__':
