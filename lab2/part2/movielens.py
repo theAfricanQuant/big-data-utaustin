608d93e7ae7cf502dedd115bb19188a1c12feb4b
Rohan Nagar <rohan.nagar@utexas.edu>
2016-03-02 22:56:01 -0600

    Build database functionality

--- lab2/part2/movielens.py
+++ lab2/part2/movielens.py
@@ -3,18 +3,27 @@ from mrjob.step import MRStep
 
 from random import randint
 
-def get_genre_numbers(field):
-    start_index = 5
+# Given an input line from a movie item, this will return a list of
+# numbers that correspond to the genres that the movie item belongs to.
+def get_genre_numbers(movie):
     genres = list()
+    start_index = 5 # Movie genres start at index 5
 
+    # There are 18 genres
     for i in xrange(18):
         index = i + start_index
-        if int(field[index]) == 1:
+
+        # If the movie is in this genre, add the number to the list
+        if int(movie[index]) == 1:
             genres.append(i)
 
     return genres
 
 
+# Builds a dict where the keys are all of the genre numbers
+# and the values are a 2-element list.
+# The first element is the count of movies in a particular genere
+# and the second is the sum of all the ratings in a particular genre.
 def build_ratings_dict():
     dictionary = dict()
 
@@ -24,6 +33,7 @@ def build_ratings_dict():
     return dictionary
 
 
+# Gives a list of k random numbers between 0 and upper_bound.
 def random_numbers(k, upper_bound):
     numbers = list()
     for i in xrange(k):
@@ -32,63 +42,74 @@ def random_numbers(k, upper_bound):
     return numbers
 
 
+# Opens the database.txt file and returns it as a dict.
+def open_database():
+    with open('database.txt', 'r') as f:
+        for line in f:
+            return eval(line)
+
+
+# Writes the database dict to a file.
+def write_to_file(db):
+    with open('database.txt', 'w') as f:
+        f.write("%s" % db)
+
+
+# Builds the user database.
+def build_database():
+    # Builds a dict where key is User ID and value is (age, genre ratings dict) tuple
+    users = dict()
+    with open('u.user', 'r') as user_file:
+       for line in user_file:
+            fields = line.split("|")
+            users[fields[0]] = (fields[1], build_ratings_dict())
+      
+    # Builds a dict where key is movie ID and value is tuple of genre numbers
+    movies = dict()
+    with open('u.item', 'r') as movie_file:
+        for line in movie_file:
+            fields = line.split("|")
+            movies[fields[0]] = get_genre_numbers(fields)
+       
+    # Goes line by line and fills updates user ratings dicts accordingly
+    with open('u.data', 'r') as data_file:
+        for line in data_file:
+            fields = line.split()
+
+            # Get the ratings dict for the user that did this rating
+            user = users[fields[0]]
+            ratings = user[1]
+
+            # Get the genre numbers for this movie that was rated
+            genres = movies[fields[1]]
+
+            # For each genre that the movie is in, add 1 to the count and the rating to the sum
+            for genre in genres:
+                current = ratings[genre]
+                current[0] += 1 # count
+                current[1] += int(fields[2]) # sum of ratings
+    
+    # Finally write this to the database file so we can use it later
+    write_to_file(users)
+
+
 class MRMovielens(MRJob):
 
     def steps(self):
 	# This function defines the steps your job will follow. If you want to chain jobs, you can just have multiple steps.
         return [
-            MRStep(mapper_init=self.build_dicts, mapper=self.k_means_mapper, reducer=self.k_means_reducer),
+            MRStep(mapper_init=self.first_step_init, mapper=self.k_means_mapper, reducer=self.k_means_reducer),
         ]
 
 
-    def build_dicts(self):
-        # Builds a dict where key is User ID and value is (age, genre ratings dict) tuple
-        self.users = dict()
-        with open('u.user', 'r') as user_file:
-            for line in user_file:
-                fields = line.split("|")
-                self.users[fields[0]] = (fields[1], build_ratings_dict())
-      
-        # Builds a dict where key is movie ID and value is tuple of genre numbers
-        self.movies = dict()
-        with open('u.item', 'r') as movie_file:
-            for line in movie_file:
-                fields = line.split("|")
-                self.movies[fields[0]] = get_genre_numbers(fields)
-       
-        # Goes line by line and fills updates user ratings dicts accordingly
-        with open('u.data', 'r') as data_file:
-            for line in data_file:
-                fields = line.split()
-
-                # Get the ratings dict for the user that did this rating
-                user = self.users[fields[0]]
-                ratings = user[1]
-
-                # Get the genre numbers for this movie that was rated
-                genres = self.movies[fields[1]]
-
-                # For each genre that the movie is in, add 1 to the count and the rating to the sum
-                for genre in genres:
-                    current = ratings[genre]
-                    current[0] += 1 # count
-                    current[1] += int(fields[2]) # sum of ratings
-        
-        print self.users
-        # Get random initial centroids (user IDs)       
-#        self.centroids = random_numbers(5, len(self.users))
-#        print self.centroids
+    def first_step_init(self):
+        print "First Step Init"
+        build_database()
 
 
     def k_means_mapper(self, _, line):
         # Calculate the distance to each centroid
-#        for user_id, value in self.users.iteritems():
-#            smallest_distance = sys.maxint
-#            closest_centroid = None
-#
-#            for centroid in self.centroids:
-#                centroid_age = self.users[centroid][0]
-        print line
+        
         # Take the closest one, and yield (centroid, user) pair
         yield "x", 1
 
